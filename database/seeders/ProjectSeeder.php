a5db2189d3d50b7fc2a24ec5820b293d5943b242
GiorgioMos <giorgiomosca2@gmail.com>
2024-01-31 19:26:28 +0100

    seeding not working

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,27 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $projects = [
+            [
+                'title' => 'progetto 1',
+                'description' => 'descrizione lunga',
+                'thumb' => 'url img',
+            ],
+            [
+                'title' => 'progetto 2',
+                'description' => 'descrizione lunga',
+                'thumb' => 'url img',
+            ],
+            [
+                'title' => 'progetto 3',
+                'description' => 'descrizione lunga',
+                'thumb' => 'url img',
+            ],
+        ];
+        foreach ($projects as $project) {
+            $newProject = new Project();
+            $newProject->fill($project);
+            $newProject->save();
+        }
     }
 }
